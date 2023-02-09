6c9c5972a09548d8230135f0a9c0fd220e6d7014
Code-ed381 <code_ed381@outlook.com>
2023-02-09 23:56:34 +0000

    Auth and CRUD complete

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -1,38 +1,27 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) { this.users = data }
-}
-
-const fsPromises = require('fs').promises;
-const path = require('path');
+const User = require('../model/User')
 
 const handleLogout =  async (req, res) => {
     //On client, also delete accessToken
-    const cookies = req.cookies; 
+    const cookies = req.cookies;  
 
     if(!cookies?.jwt) return res.sendStatus(204); //No content 
 
     const refreshToken = cookies.jwt;
 
     //Is refreshToken in DB
-    const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
+    const foundUser =  await User.findOne({ refreshToken }).exec();
     if (!foundUser) {
-        res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+        res.clearCookie('jwt', { httpOnly: true, sameSite: 'None' });
         return res.sendStatus(204); 
     }
 
     //Delete refreshToken in DB
-    const otherUsers = usersDB.users.filter(person => person.refreshToken !== foundUser.refreshToken);
-    const currentUser = {...foundUser, refreshToken: ''};
-    usersDB.setUsers([...otherUsers, currentUser]);
+    foundUser.refreshToken = '';
+    const result = await foundUser.save();
+    console.log(result)
 
-    await fsPromises.writeFile(
-        path.join(__dirname,'..', 'model', 'users.json'),
-        JSON.stringify(usersDB.users)
-    );
-
-    res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true }); //secure: true - only servers on https
+    res.clearCookie('jwt', { httpOnly: true, sameSite: 'None' }); //secure: true - only servers on https
     res.sendStatus(204)
 }
 
-module.exports = { handleLogout }
+module.exports = { handleLogout } 
